747d84ac177102b9eb0b58beb9681e2651da0aed
Greg <gregloj2001@gmail.com>
2023-11-21 15:22:07 -0500

    First commit. File uploads.

---
+++ src/TestApplication.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class TestApplication extends JFrame{
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JTextField projName;
+    private JTextPane projDescription;
+    private JList list1;
+
+    public TestApplication()
+    {
+        setContentPane(mainPanel);
+        setTitle("Greg Test");
+        setSize(400, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TestApplication testApp = new TestApplication();
+    }
+}
